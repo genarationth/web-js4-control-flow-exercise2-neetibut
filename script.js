2e56ecf042bf7ea78c849fad56485867b61a0ae8
neetibut <neetibut@gmail.com>
2024-05-14 13:14:31 +0700

    completed

---
+++ script.js
@@ -0,0 +1,31 @@
+let dayNumber = 1;
+
+function dayOfWeekTranslator(dayNumber) {
+  switch (dayNumber) {
+    case 0:
+      console.log("Sunday");
+      break;
+    case 1:
+      console.log("Monday");
+      break;
+    case 2:
+      console.log("Tuesday");
+      break;
+    case 3:
+      console.log("Wednesday");
+      break;
+    case 4:
+      console.log("Thursday");
+      break;
+    case 5:
+      console.log("Friday");
+      break;
+    case 6:
+      console.log("Saturday");
+      break;
+    default:
+      console.log("Enter a valid day numner from 0 - 6");
+  }
+}
+
+dayOfWeekTranslator(3);
